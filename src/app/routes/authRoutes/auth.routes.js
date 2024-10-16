1981ef8585bbf8ca95ddbca8eb4edffb92a6e9b0
Diego Sousa <diego7lts7@gmail.com>
2024-10-16 15:13:24 -0300

    implementando a rota de login em conjuto da autenticação via token que é enviado de volta ao cliente no login.

--- src/app/routes/authRoutes/auth.routes.js
+++ src/app/routes/authRoutes/auth.routes.js
@@ -58,4 +58,55 @@ router.post("/register", async (req, res) => {
   }
 });
 
+// rota de login
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!email) {
+    return res.status(422).json({ msg: "O email é obrigatório!" });
+  }
+
+  if (!emailPattern.test(email)) {
+    return res.status(422).json({ msg: "preencha o email corretamente" });
+  }
+
+  if (!password || password.length < 8) {
+    return res.status(422).json({ msg: "preencha sua senha corretamente!" });
+  }
+
+  const user = await UserModel.findOne({ email: email });
+
+  if (!user) {
+    return res.status(404).json({ msg: "usuario nao existe, cadastre-se!" });
+  }
+
+  const checkPassword = await bcrypt.compare(password, user.password);
+
+  if (!checkPassword) {
+    return res.status(422).json({ msg: "a senha está incorreta!" });
+  }
+
+  try {
+    const secret = process.env.SECRET;
+    const token = jwt.sign(
+      {
+        user: {
+          id: user._id,
+          name: `${user.name} ${user.lastName}`,
+          role: user.role,
+        },
+      },
+      secret
+    );
+    res.status(200).json({ msg: "usuario logado com sucesso!", token });
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ msg: "erro interno no servidor, volte mais tarde!" });
+  }
+});
+
 module.exports = router;
